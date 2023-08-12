d87ecc601314c04a75c9be852feb1cdefe7e41a6
Siphephelo Khanyile <Siphephelokhanyile@gmail.com>
2023-08-11 17:10:33 -0700

    Output fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,11 +8,14 @@ int main(void)
 {
 	int n;
 
-	for (n = '0' ; n <= '9' ; n++)
+	for (n = 0 ; n <= 9 ; n++)
 	{
-		putchar(n);
+		putchar(n + '0');
+		if (n != 9)
+		{
 		putchar(',');
 		putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
