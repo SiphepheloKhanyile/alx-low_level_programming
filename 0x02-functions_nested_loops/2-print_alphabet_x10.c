ace23721f158eb5ad8a034e267c34b457a1650aa
Siphephelo Khanyile <Siphephelokhanyile@gmail.com>
2023-08-15 16:37:38 -0700

    fixing length of my output

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * print_alphabet_x10 - Function to print the alphabets 10 times
+  * print_alphabet_x10 -print the alphabets 10 times
   * Return: void
   */
 void print_alphabet_x10(void)
