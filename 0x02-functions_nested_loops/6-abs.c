eec861488fd4d4017e8f35fb407512fbfe7b74c3
Siphephelo Khanyile <Siphephelokhanyile@gmail.com>
2023-08-15 18:19:11 -0700

    a function that computes the absolute value of an integer.

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+  * _abs - Function to calculate absolote value of integer
+  * @x: passed integer
+  * Return: z
+  */
+int _abs(int x)
+{
+	int z = abs(x);
+	return (z);
+}
