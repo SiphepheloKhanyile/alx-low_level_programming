9278c354e4af929156668a33f070a66cd9a830dd
Siphephelo Khanyile <Siphephelokhanyile@gmail.com>
2023-08-17 20:06:10 -0700

    format fix

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,17 +7,18 @@
   */
 void print_triangle(int size)
 {
-	int a, b;
+	int a, b; 
 
+	b = size;
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	while (size > 0)
 	{
-		for (a = 0; a < size; a++)
+		for (a = 0; a < b; a++)
 		{
-			_putchar((a < size -1) ? ' ' : '#');
+			_putchar((a < size - 1) ? ' ' : '#');
 		}
 		_putchar('\n');
 		size--;
