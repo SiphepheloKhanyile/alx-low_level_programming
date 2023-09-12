81c273fc2d475bc375be639abfcd1696cfaf8f75
Siphephelo Khanyile <Siphephelokhanyile@gmail.com>
2023-09-12 16:32:01 -0700

    a new type struct dog with the following elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  * dog - Structure name
+  * @name: dog name
+  * @age: dog age
+  * @owner: dog owner
+  * Description: structure to get dog details
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
