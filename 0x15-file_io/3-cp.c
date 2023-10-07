00c54a876e43ad4b07d7eebdf20c921b29712e13
Siphephelo Khanyile <Siphephelokhanyile@gmail.com>
2023-10-07 04:07:24 +0200

    changing main function's helper function

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,73 +1,88 @@
+#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <errno.h>
 
-#define BUFFER_SIZE 1024
 /**
-  * error_exit - exit function if encountered error
-  * @code: error code
-  * @message: error message
-  * Return: void
-  */
-void error_exit(int code, const char *message)
-{
-	dprintf(STDERR_FILENO, "Error: %s\n", message);
-	exit(code);
-}
-int main(int argc, char *argv[]);
-/**
-  * main - program that copies the content of a file to another file.
-  * @argc: arguement count
-  * @argv: argument var
-  * Return: 0
-  */
+ * main - program that copies the content of a file to another file.
+ * @argc: number of arguments
+ * @argv: vector arguments
+ * Return: 0 if success, otherwise 97-100
+ */
 int main(int argc, char *argv[])
 {
-	const char *file_from;
-	const char *file_to;
-	int fd_from, fd_to;
-	char buffer[BUFFER_SIZE];
-	ssize_t bytes_read, bytes_written;
+	int f_from, f_to, cStatus;
+	char buffer[1024];
+	ssize_t size;
 
 	if (argc != 3)
-	{
-		error_exit(97, "Usage: cp file_from file_to");
-	}
-	file_from = argv[1];
-	file_to = argv[2];
-	fd_from = open(file_from, O_RDONLY);
+		argError();
 
-	if (fd_from == -1)
-	{
-		error_exit(98, "Can't read from file");
-	}
-	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	f_from = open(argv[1], O_RDONLY);
+	if (f_from == -1)
+		fFrom_error(argv[1]);
 
-	if (fd_to == -1)
+	f_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	if (f_to == -1)
+		fTo_error(argv[2]);
+
+	while ((size = read(f_from, buffer, 1024)) > 0)
 	{
-		error_exit(99, "Can't write to file");
+		if (write(f_to, buffer, size) != size)
+			fTo_error(argv[2]);
 	}
-	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1)
-		{
-			error_exit(99, "Can't write to file");
-		}
-	}
-	if (bytes_read == -1)
-	{
-		error_exit(98, "Can't read from file");
-	}
-	if (close(fd_from) == -1)
-	{
-		error_exit(100, "Can't close file descriptor");
-	}
-	if (close(fd_to) == -1)
-	{
-		error_exit(100, "Can't close file descriptor");
-	}
+
+	if (size == -1)
+		fFrom_error(argv[1]);
+
+	cStatus = close(f_from);
+	if (cStatus == -1)
+		cError(f_from);
+
+	cStatus = close(f_to);
+	if (cStatus == -1)
+		cError(f_to);
+
 	return (0);
+}
+
+/**
+ * argError - Print error and exit if wrong number of arguments
+ * Return: void
+ */
+void argError(void)
+{
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	exit(97);
+}
+
+/**
+ * fFrom_error - Print error and exit if file from error
+ * @fi: name of the file
+ * Return: void
+ */
+void fFrom_error(char *fi)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", fi);
+	exit(98);
+}
+
+/**
+ * fTo_error - Print error and exit if file to error
+ * @fi: The name of the file
+ * Return: void
+ */
+void fTo_error(char *fi)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", fi);
+	exit(99);
+}
+
+/**
+ * cError - Print error and exit if close fails
+ * @fileD: The file descriptor
+ * Return: void
+ */
+void cError(int fileD)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileD);
+	exit(100);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,7 +3,15 @@
 #include <stddef.h>
 #include <sys/types.h>
 #include <string.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void argError(void);
+void fFrom_error(char *fi);
+void fTo_error(char *file);
+void cError(int fileD);
 #endif /*MAIN_H*/
