3551777f707c3dcd6b87bff2444fb9d9c026ea12
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-03-13 23:26:44 +0200

    add : readding all project tasks

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,10 +2,9 @@
 
 /**
  * linear_search - Search for the value in an array
- * @array: Pointer to the first element of the array to search
+ * @array: Points to the first element of the array to search
  * @size: Number of elements in the array
  * @value: Value to search for
- *
  * Return: First index where value is located, or -1 if it's not there
  */
 int linear_search(int *array, size_t size, int value)
